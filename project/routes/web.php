97599498d94399af555833c6205a2e1fe9adfebf
bagus <bagusyes005@gmail.com>
2021-11-12 19:18:02 +0700

    end point

--- project/routes/web.php
+++ project/routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('template');
+    return view('templates.template');
 });
 
 Route::prefix('/biodata')->group(function () {
